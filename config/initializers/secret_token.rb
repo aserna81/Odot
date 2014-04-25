af6317e1fcf312aa02ecaa8d0b916c8121638b13
aserna81 <antonio@edsurge.com>
2014-04-26 00:37:58 +0200

    Add rspec and capybara

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Odot::Application.config.secret_key_base = '62ef97ccf1db65cfe9df8e0730c90bb29fdcacbef449aa5b7722e0bcc42ba6f33971063a9dfe6c8bd3d37fa8ef1ca3e93b5e188dd02226b0fc96f42bdce932d4'
